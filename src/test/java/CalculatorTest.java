31b9fb92864be3f0f1762c3ec72b2b70280994ff
MarijanaNicic <MarijanaNicic@users.noreply.github.com>
2020-08-09 13:05:24 +0200

    Calculator Test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,6 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
@@ -24,7 +25,7 @@ public class CalculatorTest {
         DesiredCapabilities cap = new DesiredCapabilities();
 
         cap.setCapability("deviseName","SM-A202F/DS");
-        cap.setCapability("udid","R58N50J3C9N");
+        cap.setCapability("udid","192.168.1.2:5555");
         cap.setCapability("platformName","Android");
         cap.setCapability("platformVersion","10");
 
@@ -36,6 +37,47 @@ public class CalculatorTest {
         driver = new AppiumDriver<MobileElement>(url, cap);
 
         System.out.println("Application Started...");
+
+        MobileElement one = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_01"));
+        MobileElement two = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_02"));
+        MobileElement three = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_03"));
+        MobileElement four = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_04"));
+        MobileElement five = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_05"));
+        MobileElement six = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_06"));
+        MobileElement seven = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_07"));
+        MobileElement eight = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_08"));
+        MobileElement nine = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_09"));
+        MobileElement zero = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_00"));
+        MobileElement clear = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_clear"));
+        MobileElement brackets = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_parenthesis"));
+        MobileElement percent = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_percentage"));
+        MobileElement division = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_div"));
+        MobileElement multiplication = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_mul"));
+        MobileElement minus = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_sub"));
+        MobileElement plus = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_add"));
+        MobileElement plusminus = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_plusminus"));
+        MobileElement dot = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_dot"));
+        MobileElement equal = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_equal"));
+        MobileElement result = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_edt_formula"));
+
+
+        five.click();
+        seven.click();
+        clear.click();
+        eight.click();
+        plus.click();
+        three.click();
+        equal.click();
+
+
+        String res = result.getText();
+
+        System.out.println("Rezultat je "+ res);
+
+
+
+
+
 
 
     }
